136bdb3599abc5870d58b81c5ef65966fdb52849
Dontcode44 <kabutto378@gmail.com>
2022-11-09 18:07:35 -0500

    [ADD] - JWT and strategies

--- src/auth/local-auth.guard.ts
+++ src/auth/local-auth.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
+/* This class extends the AuthGuard class and uses the 'local' strategy. */
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {}
